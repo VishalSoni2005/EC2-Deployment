44b1fe78591e6a781720d65a5f158c403149966c
vishalsoni2005 <vsoni0882@email.com>
2026-05-09 13:47:33 +0530

    feat(develop): added readme guide for smooth ec2 deployment

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ import userRoutes from "./routes/user.route.js";
 import dotenv from "dotenv";
 
 dotenv.config();
-console.log(process.env.MONGO_URI);
+
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.use(express.json());
 app.use("/", userRoutes);
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(
+    "mongodb+srv://vsoni0882:%40Vishal0702@prisma-trail-bookmark-p.dr2pibz.mongodb.net/?retryWrites=true&w=majority",
+  )
   .then(() => console.log("connection done"))
   .catch(() => console.log("connection failed"));
 
